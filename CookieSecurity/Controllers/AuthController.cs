5097d001dd30af735112160462c8c3efb93149a0
Poy Chang <poypost@gmail.com>
2018-02-07 20:50:07 +0800

    加入 CookieSecurity 專案

---
+++ CookieSecurity/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CookieSecurity.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        [Route("login")]
+        [HttpGet]
+        [HttpPost]
+        public async Task<IActionResult> Login(string name, string role)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return BadRequest();
+            }
+
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, name),
+                new Claim(ClaimTypes.Role, role),
+            }, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+            return Ok();
+        }
+
+        [Route("logout")]
+        [HttpGet]
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
+
+        [Route("error")]
+        [HttpGet]
+        public IActionResult Error()
+        {
+            return BadRequest();
+        }
+    }
+}
---
+++ CookieSecurity/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CookieSecurity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.AccessDeniedPath = "/api/auth/error";
+                    options.LoginPath = "/api/auth/login";
+                });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("user", p => p.RequireAuthenticatedUser().RequireRole("user"));
+                options.AddPolicy("admin",
+                    p => p.RequireAuthenticatedUser().RequireRole("admin", "administrator"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
