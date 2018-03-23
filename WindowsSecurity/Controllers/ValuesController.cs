e42a8438d8ec6f4f647415af27bbef2c9421b7cb
Poy Chang <poypost@gmail.com>
2018-03-23 15:24:33 +0800

    加入 WindowsSecurity 專案

---
+++ WindowsSecurity/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WindowsSecurity.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values/iisAuthorize
+        [Route("iisAuthorize")]
+        [HttpGet]
+        public IActionResult IisAuthorize()
+        {
+            var name = HttpContext.User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.Name)?.Value;
+            return new ContentResult() { Content = $@"IIS authorize. {name}" };
+        }
+
+        // GET api/values/anonymous
+        [AllowAnonymous]
+        [Route("anonymous")]
+        [HttpGet]
+        public IActionResult Anonymous()
+        {
+            var name = HttpContext.User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.Name)?.Value;
+            return new ContentResult() {Content = $@"For all anonymous. {name}"};
+        }
+
+        // GET api/values/authorizeWithMvc
+        [Authorize]
+        [Route("authorize")]
+        [HttpGet]
+        public IActionResult AuthorizeWithMvc()
+        {
+            var name = HttpContext.User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.Name)?.Value;
+            return new ContentResult() {Content = $@"For all client who authorize within MVC. {name}"};
+        }
+
+        // GET api/values/onlyAdministrator
+        [Authorize(Policy = "OnlyAdministrator")]
+        [Route("onlyAdministrator")]
+        [HttpGet]
+        public IActionResult OnlyAdministrator()
+        {
+            var name = HttpContext.User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.Name)?.Value;
+            return new ContentResult() { Content = $@"For all client who authorize with Administrator role. {name}" };
+        }
+    }
+}
---
+++ WindowsSecurity/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.IISIntegration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WindowsSecurity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.Configure<IISOptions>(options => { options.AutomaticAuthentication = true; });
+            //services.AddAuthentication(IISDefaults.AuthenticationScheme);
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("OnlyAdministrator", policy =>
+                {
+                    policy.AddAuthenticationSchemes(IISDefaults.AuthenticationScheme);
+                    policy.RequireRole("Administrator");
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
